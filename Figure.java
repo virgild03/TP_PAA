c46db4972c54a737f6bc63af588cd0b796ea32e5
virgil <virgil.dinu@etu.u-paris.fr>
2024-10-13 16:23:45 +0200

    Create Figure.java

---
+++ Figure.java
@@ -0,0 +1,11 @@
+
+package up.mi.jgm.tp4;
+
+
+public abstract class Figure {
+    
+    public abstract double perimetre();
+    public abstract double surface();
+    public abstract Figure translation(Vecteur v);
+
+}
